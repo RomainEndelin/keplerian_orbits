43610575a418bfb8432e2894c1ad2097d8121677
Romain Endelin <rendelin@zendesk.com>
2021-07-28 19:39:15 +1000

    Make a minimal notebook

--- python/main.py
+++ python/main.py
@@ -1,53 +1,9 @@
-from engine.body import Body
-from engine.circular_orbit import CircularOrbit
+from engine.fixtures.system import make_system
 from engine.plot import plot_orbits
-from engine.system import System
 
 
 def main():
-    sun = Body("Sun", mass=100)
-    earth = Body("Earth", mass=5)
-    mars = Body("Mars", mass=8)
-
-    sun_earth_orbital_elements = {
-        "semimajor_axis": 10,
-        "eccentricity": 0.7,
-        "true_anomaly_at_epoch": 2,
-        "longitude_ascending_node": 0.7,
-        "inclination": 1,
-        "argument_of_periapsis": 1.5,
-    }
-    sun_earth_circular_orbital_element = {
-        "radius": sun_earth_orbital_elements["semimajor_axis"]
-    }
-
-    sun_mars_orbital_elements = {
-        "semimajor_axis": 15,
-        "eccentricity": 0.3,
-        "true_anomaly_at_epoch": 0,
-        "longitude_ascending_node": 0.2,
-        "inclination": 0,
-        "argument_of_periapsis": 0.4,
-    }
-    sun_mars_circular_orbital_element = {
-        "radius": sun_mars_orbital_elements["semimajor_axis"]
-    }
-
-    simplified = True
-    if simplified:
-        system = System(
-            sun,
-            [
-                [earth, sun_earth_circular_orbital_element],
-                [mars, sun_mars_circular_orbital_element],
-            ],
-            OrbitalEngine=CircularOrbit,
-        )
-    else:
-        system = System(
-            sun,
-            [[earth, sun_earth_orbital_elements], [mars, sun_mars_orbital_elements]],
-        )
+    system = make_system()
 
     t_val = 5
 
